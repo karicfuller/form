7e7b6dffd21fee8720696e4285c34b2daae6387a
Kari Fuller <kari.c.fuller@gmail.com>
2018-05-25 14:59:11 -0700

    add paragraph verbiage after submitting info - verbiage won't show

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function (event) {
+    var firstName = $("input#firstName").val();
+    var lastName = $("input#lastName").val();
+    var phoneNumber = $("input#phoneNumber").val();
+    var emailAddress = $("input#emailAddress").val();
+
+    $(".firstName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    $(".phoneNumber").text(phoneNumberInput);
+    $(".emailAddress").text(phoneNumberInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  };
+};
