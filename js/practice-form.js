47a2bbe3a6335b9fd54b4dc9a1d92bef59982ad9
Kari Fuller <kari.c.fuller@gmail.com>
2018-06-01 14:50:33 -0700

    format form with bootstrap, ask questions using radio buttons and box selections, and pop-up

--- js/practice-form.js
+++ js/practice-form.js
@@ -1,57 +1,21 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <script src="js/jquery-3.3.1.js"></script>
-    <script src="js/scripts.js"></script>
-    <title>Make an Appointment</title>
-  </head>
-  <body>
-    <div class="container">
-      <h1>Enter Your Info to Make an Appointment</h1>
-      <div id="blanks"
-        <form>
-          <div class="form-group">
-            <label for="firstName">First Name</label>
-            <input id="firstName" class="form-control" type="text">
-          </div>
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
 
-          <div class="form-group">
-            <label for="lastName">Last Name</label>
-            <input id="lastName" class="form-control" type="text">
-          </div>
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
-          <div class="form-group">
-            <label for="phoneNumber">Phone Number</label>
-            <input id="phoneNumber" class="form-control" type="text">
-          </div>
+    $("#story").show();
 
-          <div class="form-group">
-            <label for="emailAddress">Email Address</label>
-            <input id="emailAddress" class="form-control" type="text">
-          </div>
-
-          <button type="submit" class="btn">Show me the story!</button>
-        </form>
-      </div>
-
-    <div id="story">
-      <h1>We Will Contact You!</h1>
-      <p>Hi there <span class="firstName"></span> <span class="lastName"></span> thanks for your interest! We will contact you at your email address,<span class="emailAddress"></span> within the next 24 hours. If we are unable to reach you at <span class="emailAddress"></span> we will give you a call at <span class="phoneNumber"></span>.</p>
-    </div>
-  </body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-</html>
+    event.preventDefault();
+  });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,1 @@
-$(document).ready(function() {
-  $("#blanks form").submit(function (event) {
-    var firstName = $("input#firstName").val();
-    var lastName = $("input#lastName").val();
-    var phoneNumber = $("input#phoneNumber").val();
-    var emailAddress = $("input#emailAddress").val();
-
-    $(".firstName").text(firstNameInput);
-    $(".lastName").text(lastNameInput);
-    $(".phoneNumber").text(phoneNumberInput);
-    $(".emailAddress").text(phoneNumberInput);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
-});
+alert("Hi There! Let's Find Your Dream Vacation!");
